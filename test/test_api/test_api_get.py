a57675faa65c729b0e4fd7381d2fb5ffcec723c1
Diya <diya.elsa@yahoo.com>
2026-01-19 22:35:59 +0400

    updating recordings and htmpt reports

--- test/test_api/test_api_get.py
+++ test/test_api/test_api_get.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 def test_api_page_load():
     with sync_playwright() as p:
         request = p.request.new_context()
-        response=request.get("https://m.twitch.tv/directory")
+        response=request.get("https://www.twitch.tv/")
         print(response.status)
         print(response.text)
         assert response.status == 200
